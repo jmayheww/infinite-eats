bb35b1d11e18a089c86c4f793c2f50029fd86550
Joshua <joshsmayhew@gmail.com>
2023-06-03 14:15:26 +0700

    create api routes for user signup, login, current user, and logout, each route correctly designates http verb, route path, and controller action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :users, only: %i[show create update destroy]
+
+    post '/signup', to: 'users#create'
+    post '/login', to: 'sessions#create'
+    get '/me', to: 'users#show'
+    delete '/logout', to: 'sessions#destroy'
   end
   # Routing logic: fallback requests for React Router.
   get '/hello', to: 'application#hello_world'
