2378b9a9c7aaf1d70b9b4764f208a14a1f24d539
Joshua <joshsmayhew@gmail.com>
2023-06-03 14:36:10 +0700

    create new functions in userAuth.js to relay client requests to backend routes for current user, user login, user signup, and user logout, create helper functions currentUserResp and authResponseHandler to update user state, loading state, errors state, and navigate user to appropriate component on frontend

--- client/src/context/userAuth.js
+++ client/src/context/userAuth.js
@@ -24,11 +24,87 @@ export const UserProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
 
+  const fetchCurrentUser = () => {
+    fetch("/api/me").then(currentUserResp);
+  };
+
+  const loginUser = (formData) => {
+    fetch("/api/login", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(formData),
+    }).then(authResponseHandler);
+  };
+
+  const signupUser = (formData) => {
+    fetch("/api/signup", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(formData),
+    }).then(authResponseHandler);
+  };
+
+  const logoutUser = () => {
+    setErrors([]);
+    fetch("/api/logout", { method: "DELETE" }).then(() => {
+      setUser(null);
+      setShowLogin(true);
+      navigate("/login");
+    });
+  };
+
+  const currentUserResp = (r) => {
+    if (r.ok) {
+      r.json().then((user) => {
+        setUser(user);
+      });
+    } else {
+      setUser(null);
+      navigate("/login");
+    }
+  };
+
+  const authResponseHandler = (r) => {
+    if (r.ok) {
+      setIsLoading(false);
+      r.json().then((user) => {
+        setUser(user);
+        navigate("/home");
+      });
+    } else {
+      setIsLoading(false);
+      r.json().then((data) => {
+        if (data.errors) {
+          setErrors(data.errors);
+        } else {
+          setErrors([data.error]);
+        }
+      });
+    }
+  };
+
+  const buttonClickResponseHandler = (buttonType) => {
+    if (buttonType === "login") {
+      setShowLogin(true);
+      setErrors([]);
+      navigate("/login");
+    } else {
+      setShowLogin(false);
+      setErrors([]);
+      navigate("/signup");
+    }
+  };
+
   return (
     <UserContext.Provider
       value={{
         user,
         setUser,
+        fetchCurrentUser,
+        loginUser,
+        signupUser,
+        logoutUser,
+        buttonClickResponseHandler,
         headers,
         errors,
         setErrors,
