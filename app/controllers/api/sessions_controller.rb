1275485ac9ad53b6bcbe51c394ca7d9f045a91f5
Joshua <joshsmayhew@gmail.com>
2023-06-03 14:12:27 +0700

    generate sessions controller to handle auth requests in rails backend

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Api::SessionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_unauthorized_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    user&.authenticate(params[:password])
+    session[:user_id] = user.id
+    render json: user, status: :created
+  end
+
+  private
+
+  def render_unauthorized_response
+    render json: { error: 'Invalid email or password' }, status: :unauthorized
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+  end
+end
