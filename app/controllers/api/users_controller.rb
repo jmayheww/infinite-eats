a7feff41ba9cf3aebec4697c6e97caec88c79055
Joshua <joshsmayhew@gmail.com>
2023-06-03 14:59:18 +0700

    update render_record_not_found_response error message and set to return unauthorized status

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,6 +5,12 @@ class Api::UsersController < ApplicationController
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotUnique, with: :render_unique_violation_response
 
+  def create
+    new_user = User.create!(user_params)
+    session[:user_id] = new_user.id
+    render json: new_user, status: :created
+  end
+
   def show
     render json: @current_user, status: :ok
   end
@@ -15,16 +21,20 @@ class Api::UsersController < ApplicationController
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
-  def render_record_not_found_response(_exception)
-    render json: { error: 'Not authorized.' }, status: :unauthorized
+  def user_params
+    params.permit(:username, :email, :password, :password_confirmation)
+  end
+
+  def render_record_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :unauthorized
   end
 
   def render_unprocessable_entity_response(exception)
     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 
-  def render_unique_violation_response(_exception)
-    error_message = "#{model} already exists"
+  def render_unique_violation_response(exception)
+    error_message = "#{exception.model} already exists"
     render json: { error: error_message }, status: :unprocessable_entity
   end
 end
