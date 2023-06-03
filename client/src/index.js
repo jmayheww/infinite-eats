3e94a517bdf0ca8caee2b12231cc445dcbe4e28b
Joshua <joshsmayhew@gmail.com>
2023-06-03 15:14:29 +0700

    update index.js, import UserProvider from userAuth context, import BrowserRouter as Router from react-router-dom, nest App within Router and Context and thus provide access to both

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "../src/components/App";
+import { BrowserRouter as Router } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { UserProvider } from "./context/userAuth";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
