d628f57a2467bedd36f06ecba1feaf221915142f
Joshua <joshsmayhew@gmail.com>
2023-06-05 12:17:37 +0700

    edit tailwind.config.css file add paths to template files, setup tailwind styling in client

--- client/src/pages/AuthenticationPage.js
+++ client/src/pages/AuthenticationPage.js
@@ -6,31 +6,22 @@ import SignupForm from "../components/SignupForm";
 function AuthenticationPage() {
   const { showLogin, buttonClickResponseHandler } = useContext(UserContext);
   return (
-    <div className="Auth">
-      <h1>Authentication Page</h1>
+    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-8">Authentication Page</h1>
 
-      {showLogin ? (
-        <>
-          <LoginForm />
+      {showLogin ? <LoginForm /> : <SignupForm />}
 
-          <p>
-            Don't have an account? &nbsp;
-            <button onClick={() => buttonClickResponseHandler("signup")}>
-              Sign Up
-            </button>
-          </p>
-        </>
-      ) : (
-        <>
-          <SignupForm />
-          <p>
-            Already have an account? &nbsp;
-            <button onClick={() => buttonClickResponseHandler("login")}>
-              Log In
-            </button>
-          </p>
-        </>
-      )}
+      <p className="text-red-600">
+        {showLogin ? "Don't have an account? " : "Already have an account? "}
+        <button
+          className="text-blue-500 underline ml-1"
+          onClick={() =>
+            buttonClickResponseHandler(showLogin ? "signup" : "login")
+          }
+        >
+          {showLogin ? "Sign Up" : "Log In"}
+        </button>
+      </p>
     </div>
   );
 }
