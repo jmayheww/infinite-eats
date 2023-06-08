220225e7799577460cdbf8a61c057ea872b553a6
Joshua <joshsmayhew@gmail.com>
2023-06-08 15:54:12 +0700

    import context hook in App.js, destructure user and fetchCurrentUser from UserContext, execute fetchCurrentUser in useEffect to check for existing user on app mount

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,16 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Routes, Route } from "react-router-dom";
+import UserContext from "../context/userAuth";
 
 import NavBar from "./NavBar";
 import AuthenticationPage from "../pages/AuthenticationPage";
 
 function App() {
   const [count, setCount] = useState(0);
+  const { user, logoutUser, fetchCurrentUser } = useContext(UserContext);
+  console.log("user: ", user);
 
   useEffect(() => {
     fetch("/hello")
       .then((r) => r.json())
       .then((data) => setCount(data.count));
+
+    // check for existing user
+    fetchCurrentUser();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
