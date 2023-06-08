dc6b607a06519432578325e88d91df3c7be70c90
Joshua <joshsmayhew@gmail.com>
2023-06-08 16:18:08 +0700

    create submit button and handleFormSubmission function in LoginSingupForm to handle user form submissions, edit styling to make the submit button blend more seamlessly into the form input fields, removed front end required attribute from input tags in lieu of backend validations and error handling

--- client/src/components/LoginSignupForm.js
+++ client/src/components/LoginSignupForm.js
@@ -6,8 +6,13 @@ function LoginSignupForm() {
   const { userAuthInput, handleUserAuthInput } = useContext(UserContext);
   const { pathname } = useLocation();
 
+  function handleFormSubmission(e) {
+    e.preventDefault();
+    console.log("form submitted");
+  }
+
   return (
-    <form className="mt-8 space-y-6">
+    <form className="mt-8 space-y-6" onSubmit={handleFormSubmission}>
       <div className="rounded-md shadow-sm -space-y-px">
         <div>
           <label htmlFor="email-address" className="sr-only">
@@ -19,10 +24,9 @@ function LoginSignupForm() {
             type="email"
             autoComplete="email"
             onChange={handleUserAuthInput}
-            required
             className={`appearance-none ${
               pathname === "/signup" ? "rounded-t-md" : "rounded-t-md"
-            } relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary focus:z-10 sm:text-sm font-semibold`}
+            } relative block w-full px-3 py-2 border border-primary placeholder-primary text-primary focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent focus:z-10 sm:text-sm font-opensans`}
             placeholder="Email address"
           />
         </div>
@@ -36,10 +40,9 @@ function LoginSignupForm() {
             type="password"
             autoComplete="current-password"
             onChange={handleUserAuthInput}
-            required
-            className={`appearance-none ${
-              pathname === "/signup" ? "rounded-none" : "rounded-b-md"
-            } relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary focus:z-10 sm:text-sm font-semibold`}
+            className={
+              "relative block w-full px-3 py-2 border border-primary placeholder-primary text-primary focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent focus:z-10 sm:text-sm font-opensans"
+            }
             placeholder="Password"
           />
         </div>
@@ -54,13 +57,21 @@ function LoginSignupForm() {
               id="password_confirmation"
               value={userAuthInput.password_confirmation}
               onChange={handleUserAuthInput}
-              className={`appearance-none ${
-                pathname === "/signup" ? "rounded-b-md" : "rounded-none"
-              } relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-1 focus:ring-primary focus:border-primary focus:z-10 sm:text-sm font-semibold`}
+              className={
+                "relative block w-full px-3 py-2 border border-primary placeholder-primary text-primary focus:outline-none focus:ring-1 focus:ring-accent focus:border-accent focus:z-10 sm:text-sm font-opensans"
+              }
               placeholder="Confirm Password"
             />
           </div>
         )}
+        <div>
+          <button
+            type="submit"
+            className="group relative w-full flex justify-center py-2 px-4 border border-primary rounded-b-md text-sm font-opensans text-white bg-primary hover:bg-secondary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent"
+          >
+            {pathname === "/login" ? "Log In" : "Sign Up"}
+          </button>
+        </div>
       </div>
     </form>
   );
