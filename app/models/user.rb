20c03a00f539d67975b2ce7e2812a4185c637aba
Joshua <joshsmayhew@gmail.com>
2023-06-09 12:20:57 +0700

    update user model validations has_secure_passsword macro already triggers validations for password and pass confirmation presence, change additional pass validation to set min char length, remove redundant error messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
 end
