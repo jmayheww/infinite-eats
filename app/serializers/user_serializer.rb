ce3a82e0e460d9da028e3e9274eca65d952cde74
Joshua <joshsmayhew@gmail.com>
2023-06-15 14:37:05 +0700

    update Home.js page component to include tagling descriptions of three core app features, update utility classes to alter page styling, make login and sign up section conditional on user being not logged in

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :password_digest, :created_at, :updated_at
+  attributes :id, :email, :password_digest, :created_at, :updated_at, :username
 end
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,21 @@
 import React, { useContext } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import UserContext from "../context/userAuth";
 
 function Home() {
+  const { user } = useContext(UserContext);
+
   return (
     <div className="bg-tertiary min-h-screen flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl text-center">
         <h1 className="text-5xl font-extrabold text-primary mb-6">
-          Welcome to Infinite Eats!
+          {user?.username
+            ? `Welcome, ${user?.username}!`
+            : "Welcome to Infinite Eats!"}
         </h1>
         <p className="text-2xl text-secondary mb-8">
-          The ultimate solution for managing your fridge inventory and
-          automating restocking.
+          Your fridge, your way. Infinite Eats is the ultimate fridge management
+          app.
         </p>
         {/* Features section */}
         <section className="mb-12">
@@ -22,51 +26,53 @@ function Home() {
                 Inventory Tracking
               </h3>
               <p className="text-secondary">
-                Keep track of your fridge items and easily manage your inventory
-                with our intuitive tracking system.
+                Know what you have in your fridge. Track and manage your
+                inventory in real-time.
               </p>
             </div>
             <div className="w-full md:w-1/2 lg:w-1/3 p-4">
               <h3 className="text-xl font-semibold text-primary mb-2">
-                Automated Restocking
+                Browse and Add Stock from Reputable Vendors
               </h3>
               <p className="text-secondary">
-                Set up custom or automated orders to ensure your fridge is
-                always stocked based on your preferences and needs.
+                Access a wide selection of high-quality products from reputable,
+                big-name retailers. Easily add stock to your fridge with
+                confidence.
               </p>
             </div>
             <div className="w-full md:w-1/2 lg:w-1/3 p-4">
               <h3 className="text-xl font-semibold text-primary mb-2">
-                Meal Planning
+                Customizable and Automated Restocking
               </h3>
               <p className="text-secondary">
-                Plan your meals ahead by creating customized meal plans based on
-                your available ingredients.
+                Never run out of your favorite foods again! Customize your
+                restocking preferences and let Infinite Eats handle the rest.
               </p>
             </div>
-            {/* Add more feature cards as needed */}
           </div>
         </section>
         {/* Call-to-action section */}
-        <section>
-          <h2 className="text-3xl font-bold text-primary mb-4">
-            Get Started Today
-          </h2>
-          <div className="flex items-center justify-center space-x-4">
-            <Link
-              to="/signup"
-              className="py-3 px-6 text-white bg-primary rounded-md hover:bg-secondary transition-colors duration-300"
-            >
-              Sign Up
-            </Link>
-            <Link
-              to="/login"
-              className="py-3 px-6 border border-primary text-primary rounded-md hover:bg-primary hover:text-white transition-colors duration-300"
-            >
-              Log In
-            </Link>
-          </div>
-        </section>
+        {!user && (
+          <section>
+            <h2 className="text-3xl font-bold text-primary mb-4">
+              Get Started Today
+            </h2>
+            <div className="flex items-center justify-center space-x-4">
+              <Link
+                to="/signup"
+                className="py-3 px-6 text-white bg-primary rounded-md hover:bg-secondary transition-colors duration-300"
+              >
+                Sign Up
+              </Link>
+              <Link
+                to="/login"
+                className="py-3 px-6 border border-primary text-primary rounded-md hover:bg-primary hover:text-white transition-colors duration-300"
+              >
+                Log In
+              </Link>
+            </div>
+          </section>
+        )}
       </div>
     </div>
   );
