437092f9520b9098999aa93a9085af1501e6df74
Joshua <joshsmayhew@gmail.com>
2023-06-15 16:03:34 +0700

    update stock taglines in Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,42 +7,42 @@ function Home() {
 
   return (
     <div className="bg-tertiary min-h-screen flex flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-3xl text-center">
+      <div className="max-w-3xl text-center mt-20">
         <h1 className="text-5xl font-extrabold text-primary mb-6">
           {user?.username
             ? `Welcome, ${user?.username}!`
             : "Welcome to Infinite Eats!"}
         </h1>
-        <p className="text-2xl text-secondary mb-8">
+        <p className="text-xl text-secondary mb-8">
           Your fridge, your way. Infinite Eats is the ultimate fridge management
           app.
         </p>
         {/* Features section */}
         <section className="mb-12">
-          <h2 className="text-3xl font-bold text-primary mb-4">Features</h2>
-          <div className="flex flex-wrap justify-center">
-            <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-              <h3 className="text-xl font-semibold text-primary mb-2">
-                Inventory Tracking
+          <h2 className="text-3xl font-bold text-primary mb-6">Features</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="p-8 border rounded-lg shadow-lg h-full">
+              <h3 className="text-xl font-semibold text-primary mb-4">
+                Track Inventory
               </h3>
               <p className="text-secondary">
                 Know what you have in your fridge. Track and manage your
                 inventory in real-time.
               </p>
             </div>
-            <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-              <h3 className="text-xl font-semibold text-primary mb-2">
-                Browse and Add Stock from Reputable Vendors
+            <div className="p-8 border rounded-lg shadow-lg h-full">
+              <h3 className="text-xl font-semibold text-primary mb-4">
+                Access Vendors
               </h3>
               <p className="text-secondary">
                 Access a wide selection of high-quality products from reputable,
-                big-name retailers. Easily add stock to your fridge with
+                big-name vendors. Easily add stock to your fridge with
                 confidence.
               </p>
             </div>
-            <div className="w-full md:w-1/2 lg:w-1/3 p-4">
-              <h3 className="text-xl font-semibold text-primary mb-2">
-                Customizable and Automated Restocking
+            <div className="p-8 border rounded-lg shadow-lg h-full">
+              <h3 className="text-xl font-semibold text-primary mb-4">
+                Restock Fridge
               </h3>
               <p className="text-secondary">
                 Never run out of your favorite foods again! Customize your
