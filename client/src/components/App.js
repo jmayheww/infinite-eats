22b4a54215b7983e6a09ef8f33d59ae5ec361397
Joshua <joshsmayhew@gmail.com>
2023-06-17 14:20:40 +0700

    remove padding in LandingPage, create new AsyncFeaturesPage variable to store lazy loaded features page in App.js, write new exact path /features to render FeaturesPage in react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import NavBar from "./NavBar";
 
 const AsyncLandingPage = React.lazy(() => import("../pages/LandingPage"));
 const AsyncAuthPage = React.lazy(() => import("../pages/AuthenticationPage"));
+const AsyncFeaturesPage = React.lazy(() => import("../pages/FeaturesPage"));
 
 function App() {
   const { user, fetchCurrentUser, isLoading } = useContext(UserContext);
@@ -27,16 +28,15 @@ function App() {
         <Routes>
           <Route exact path="/testing" element={<h1>Test Route</h1>} />
           <Route exact path="/myaccount" element={<h1>My Account Page</h1>} />
+          <Route exact path="/features" element={<AsyncFeaturesPage />} />
           <Route path="/landing" element={<AsyncLandingPage />} />
           <Route exact path="/" element={<Navigate to="/landing" replace />} />
-
           {!user && (
             <>
               <Route exact path="/login" element={<AsyncAuthPage />} />
               <Route exact path="/signup" element={<AsyncAuthPage />} />
             </>
           )}
-
           <Route path="*" element={<Navigate to="/landing" />} />
         </Routes>
       </Suspense>
--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { Element } from "react-scroll";
-
 import WelcomeSection from "../sections/WelcomeSection";
 
 function LandingPage() {
   return (
-    <div className="pt-16">
-      <Element name="welcome">
-        <WelcomeSection />
-      </Element>
+    <div>
+      <WelcomeSection />
     </div>
   );
 }
