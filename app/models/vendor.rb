b32924b53af7b64bbcdd34201cf7c722c9fdec33
Joshua <joshsmayhew@gmail.com>
2023-06-22 21:15:19 +0700

    update validations in vendor and product models

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,19 +1,7 @@
 class Vendor < ApplicationRecord
   has_many :vendors_products
   has_many :products, through: :vendors_products
-  has_many :orders
-  has_many :users, through: :orders
 
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :phone_number, presence: true
-  validates :address, presence: true
-  validates :city, presence: true
-  validates :state, presence: true, length: { is: 2 }
-  validates :zip_code, presence: true, length: { is: 5 }
-  validates :website, presence: true
-  validates :description, presence: true
-  validates :delivery_schedule, presence: true
-  validates :average_rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  validates :logo_image_url, presence: true
+  validates_presence_of :name, :email, :phone_number, :address, :city, :state, :zip_code, :website, :description,
+                        :delivery_schedule, :average_rating, :logo_image_url
 end
